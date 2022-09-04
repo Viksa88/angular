4f642ae02670fee07026e87604f3f489f12fc6ab
viksa88 <vic.bud22@gmail.com>
2022-09-04 10:11:55 +0300

    update

--- royal-master/README.md
+++ royal-master/README.md
@@ -1,5 +1,5 @@
-Install
+Install:
 Run `npm install` for installing packages for the project
 
-Development server
+Development server:
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`.
--- victoria-master/src/services/email.service.js
+++ victoria-master/src/services/email.service.js
@@ -3,7 +3,7 @@ const config = require('../config/config');
 const logger = require('../config/logger');
 
 const transport = nodemailer.createTransport(config.email.smtp);
-/* istanbul ignore next */
+
 if (config.env !== 'test') {
   transport
     .verify()
@@ -12,7 +12,6 @@ if (config.env !== 'test') {
 }
 
 /**
- * Send an email
  * @param {string} to
  * @param {string} subject
  * @param {string} text
@@ -24,14 +23,13 @@ const sendEmail = async (to, subject, text) => {
 };
 
 /**
- * Send reset password email
  * @param {string} to
  * @param {string} token
  * @returns {Promise}
  */
 const sendResetPasswordEmail = async (to, token) => {
   const subject = 'Reset password';
-  // replace this url with the link to the reset password page of your front-end app
+ 
   const resetPasswordUrl = `http://link-to-app/reset-password?token=${token}`;
   const text = `Dear user,
 To reset your password, click on this link: ${resetPasswordUrl}
@@ -40,14 +38,12 @@ If you did not request any password resets, then ignore this email.`;
 };
 
 /**
- * Send verification email
  * @param {string} to
  * @param {string} token
  * @returns {Promise}
  */
 const sendVerificationEmail = async (to, token) => {
   const subject = 'Email Verification';
-  // replace this url with the link to the email verification page of your front-end app
   const verificationEmailUrl = `http://link-to-app/verify-email?token=${token}`;
   const text = `Dear user,
 To verify your email, click on this link: ${verificationEmailUrl}
